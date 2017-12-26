941a565c99b638bb0e9db15f1e9b608495e36adf
overwraith <cnblock@cox.net>
2017-12-26 11:02:29 -0600

    CloudLogger
    A cloud logging service loggs to a single pc.

---
+++ CloudLogger/CloudLogger/CloudLoggerModule.cs
@@ -0,0 +1,56 @@
+﻿/*Author: Cameron Block*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Nancy;
+
+namespace CloudLogger {
+
+    /// <summary>
+    /// Log cloud application events to single cloud computer. 
+    /// </summary>
+    public class CloudLoggerModule : NancyModule {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger("CloudLogger");
+
+        /// <summary>
+        /// Constructor cloud logger module. 
+        /// </summary>
+        public CloudLoggerModule() : base("/CloudLogger") {
+            Post["/Debug/{message}"] = parameters => { return Debug(parameters.message); };
+            Post["/Error/{message}"] = parameters => { return Error(parameters.message); };
+        }//end method
+
+        /// <summary>
+        /// Log multiple computers to a single location on the network. 
+        /// </summary>
+        /// <param name="message"></param>
+        public bool Debug(String message) {
+            try {
+                log.Debug(message);
+                return true;
+            }
+            catch (Exception ex) {
+                return false;
+            }
+        }//end method
+
+        /// <summary>
+        /// Log error messages from multiple locations to a single computer on the network. 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public bool Error(String message) {
+            try {
+                log.Error(message);
+                return true;
+            }
+            catch (Exception ex) {
+                return false;
+            }
+        }//end method
+
+    }//end class
+
+}//end namespace
---
+++ CloudLogger/CloudLogger/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Nancy.Owin;
+using Microsoft.AspNetCore.Builder;
+using Owin;
+
+[assembly: OwinStartup(typeof(CloudLogger.Startup))]
+
+namespace CloudLogger {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseNancy();
+        }
+    }
+}
